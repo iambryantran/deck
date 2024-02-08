642f77d16dab06bee994970eaf80005e266bb54c
sepan mohammed <sepanmustafa@gmail.com>
2024-02-07 18:08:53 -0600

    Worked on the Login page, redesgined the the Login Page to match up with the Signup page, LoginPage was using plain CSS and singup page was using material UI. Changed the login design with material UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,7 +7,17 @@ import { Navigate } from "react-router-dom";
 import '../Styles/LoginForm.css'
 import { Link } from 'react-router-dom'
 import { FaUser, FaLock } from "react-icons/fa";
-import { Stack } from '@mui/material';
+// import { Stack } from '@mui/material';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 import Page from "../components/Page";
 import AuthService from "../utils/auth";
@@ -67,63 +77,108 @@ export default function Login() {
   }
 
   return (
-      <Page isProtected={false} headContent={headContent} bodyStyles={{ backgroundColor: 'seagreen' }}>
-        <div>hi</div>
-        <Stack sx={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexGrow: 1
-        }}>
-          <form className= "wrapper" style={styles.form} onSubmit={handleFormSubmit}>
-            <h1 className="loginText">Login</h1>
 
-            <div className="input-box"> 
-            <input
-              placeholder="Email"
-              name="email"
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <FaUser className="icon"/>
-            </div>
-
-            <div className="input-box">
-            <input
-              placeholder="Password"
-              name="password"
-              type="password"
-              value={formState.password}
-              onChange={handleChange}
-            />
-            <FaLock className="icon"/>
-            </div>
-
-            <div className="remember-forgot">
-              <label >
-                <input 
-                type="checkbox" 
-                />
-                Remember me
-              </label>
-              <Link className="forgotLink"> Forgot password?
-              </Link>
-            </div>
-
+      <Page isProtected={false} headContent={headContent} bodyStyles={{ backgroundColor: 'seagreen' }}> 
+        <Container component="main" maxWidth="xs">
+          <Box
+            
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              backgroundColor: '#408C93',
+              border: '2px solid rgba(255, 255, 255, .2)',
+              backdropFilter: 'blur(30px)',
+              // borderWidth: '1px',
+              width: '420px',
+              borderRadius: '10px',
+              padding: '30px 40px',
+              // color: '#fff',
+              boxShadow: '0 0 10px  rgba(0, 0, 0, .2)',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          > 
+            <Avatar sx={{ m: 1, bgcolor: 'black' }}></Avatar>
+            <Typography component="h1" variant="h5"
+            sx={{
+              fontSize: "36px",
+              marginBottom: '20px',
+              color: '#fff'
+            }}>
+              Login
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleFormSubmit} sx={{ mt: 3, color: '#fff',}} > 
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    // variant="filled"
+                    id="email"
+                    label="Email"
+                    autoComplete="email"
+                    placeholder="Email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                    InputProps={{
+                      style: {
+                        borderRadius: "15px",
+                        color: 'white',
+                        accentColor: '#fff',
+                        backgroundColor: '#235157',
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    fullWidth
+                    id="password"
+                    // variant="filled"
+                    label="Password"
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                    autoComplete="new-password"
+                    InputProps={{
+                      style: {
+                        borderRadius: "15px",
+                        color: 'white',
+                        accentColor: '#fff',
+                        backgroundColor: '#235157',
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="Remember me" sx={{color: 'white'}}
+                  />
+                </Grid>
+            </Grid>
             {loading ? (
-              <button type="submit" disabled={true} style={styles.submitBtn}>
+              <Button  fullWidth type="submit" variant="contained" sx={{ mt: 3, mb: 2, backgroundColor: '#6DA5C0' }} disabled={true} style={styles.submitBtn}>
                 Loading...
-              </button>
+              </Button>
             ) : (
-              <button type="submit" style={styles.submitBtn}>
+              <Button fullWidth type="submit"  variant="contained" sx={{ mt: 3, mb: 2, backgroundColor: '#6DA5C0' }} style={styles.submitBtn}>
                 Login
-              </button>
+              </Button>
             )}
-            <div className="register-link">
-              <p className="pText">Don't have an account? <Link to={'/signup'} className="register"> Register</Link></p>
-            </div>
-          </form>
-        </Stack>
+              <Grid container justifyContent="">
+                <Grid item> Don't have an account?
+                  <Link to={"/signup"} variant="body2" sx={{ color: ''}}>
+                  Register
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
         {error && <h3>{error.message}</h3>}
       </Page>
   );
