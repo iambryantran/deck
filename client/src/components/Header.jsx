8e98c3d94720df812c937edd31c359ff00fc6938
brandonlambrecht <brandonjlambrecht@gmail.com>
2024-02-08 20:28:52 -0600

    added Modal to dashboard, changed title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,27 +2,22 @@ import { Link } from "react-router-dom";
 import AuthServices from "../utils/auth";
 import { useSelector } from "react-redux";
 import { getUser } from "../redux/slices/userSlice";
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-import AccountCircle from '@mui/icons-material/AccountCircle';
-
-
-
-
-
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const styles = {
   container: {
@@ -48,90 +43,86 @@ const styles = {
   },
 };
 
-
 export default function Header() {
-
-  
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-  
+
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  
-  
-  
+
   const { isAuthenticated } = useSelector(getUser());
-  
+
   const handleLogout = (e) => {
     AuthServices.logout();
   };
-  
-  const settings = ['Profile', 'Account', 'Logout',];
-  
+
+  const settings = ["Profile", "Account", "Logout"];
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-        <Toolbar sx={{ backgroundColor: '#408C93'}}>
-        <IconButton
+      <AppBar position="static">
+        <Toolbar sx={{ backgroundColor: "#408C93" }}>
+          <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-        >
+          >
             <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Project-3 Starter Code
-        </Typography>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Career Cache
+          </Typography>
 
-              <div style={styles.buttonDiv}>
+          <div style={styles.buttonDiv}>
             {!isAuthenticated && (
-              <Link to={"/signup"}> 
-                <Button sx={{color:'white' }} >Sign Up</Button>
+              <Link to={"/signup"}>
+                <Button sx={{ color: "white" }}>Sign Up</Button>
               </Link>
             )}
             {!isAuthenticated && (
               <Link to={"/login"}>
-                <Button sx={{color:'white' }}
-                >Login</Button>
+                <Button sx={{ color: "white" }}>Login</Button>
               </Link>
             )}
-          </div>  
+          </div>
 
           {isAuthenticated && (
-        <Box sx={{ flexGrow: 0, }} >
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, color:"inherit", size: "large"}}>
-                {/* <Avatar alt="Remy Sharp" src="" /> */}
-                <AccountCircle />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px', justifyContent: 'end'}}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >        
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting} </Typography>
-                </MenuItem>
-                
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ p: 0, color: "inherit", size: "large" }}
+                >
+                  {/* <Avatar alt="Remy Sharp" src="" /> */}
+                  <AccountCircle />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px", justifyContent: "end" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting} </Typography>
+                  </MenuItem>
                 ))}
                 {/* <MenuItem  onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">
@@ -142,30 +133,29 @@ export default function Header() {
                 </MenuItem> */}
               </Menu>
               {isAuthenticated && (
-                  <Link to={"/about"}>
-                    <Button sx={{color:'white' }} >About</Button>
-                  </Link>
-                )}
-                {isAuthenticated && (
-                  <Link to={"/dashboard"}>
-                    <Button sx={{color:'white' }} >Dashboard</Button>
-                  </Link>
-                )}
-                {isAuthenticated && (
-                  <Link to={"/gallery"}>
-                  <Button sx={{color:'white' }} >Gallery</Button>
-                  </Link>
-                )}
-                {isAuthenticated && (
-                <Button onClick={handleLogout} sx={{color:'white' }} >
+                <Link to={"/dashboard"}>
+                  <Button sx={{ color: "white" }}>Dashboard</Button>
+                </Link>
+              )}
+              {isAuthenticated && (
+                <Link to={"/about"}>
+                  <Button sx={{ color: "white" }}>About</Button>
+                </Link>
+              )}
+              {isAuthenticated && (
+                <Link to={"/gallery"}>
+                  <Button sx={{ color: "white" }}>Gallery</Button>
+                </Link>
+              )}
+              {isAuthenticated && (
+                <Button onClick={handleLogout} sx={{ color: "white" }}>
                   Logout
                 </Button>
               )}
-
             </Box>
-            )}
+          )}
         </Toolbar>
-    </AppBar>
+      </AppBar>
     </Box>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,20 +4,20 @@ import { LOGIN_USER } from "../graphql/mutations";
 import { useSelector } from "react-redux";
 import { getUser } from "../redux/slices/userSlice";
 import { Navigate } from "react-router-dom";
-import '../Styles/LoginForm.css'
-import { Link } from 'react-router-dom'
+import "../Styles/LoginForm.css";
+import { Link } from "react-router-dom";
 import { FaUser, FaLock } from "react-icons/fa";
 // import { Stack } from '@mui/material';
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 import Page from "../components/Page";
 import AuthService from "../utils/auth";
@@ -35,7 +35,7 @@ const styles = {
 
 const headContent = (
   <>
-    <title>Change Me! - Login</title>
+    <title>Career Cache</title>
     <meta name="description" content="Login for Project-3 Starter Code." />
   </>
 );
@@ -77,109 +77,137 @@ export default function Login() {
   }
 
   return (
-
-      <Page isProtected={false} headContent={headContent} bodyStyles={{ backgroundColor: 'seagreen' }}> 
-        <Container component="main" maxWidth="xs">
-          <Box
-            
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              backgroundColor: '#408C93',
-              border: '2px solid rgba(255, 255, 255, .2)',
-              backdropFilter: 'blur(30px)',
-              // borderWidth: '1px',
-              width: '420px',
-              borderRadius: '10px',
-              padding: '30px 40px',
-              // color: '#fff',
-              boxShadow: '0 0 10px  rgba(0, 0, 0, .2)',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          > 
-            <Avatar sx={{ m: 1, bgcolor: 'black' }}></Avatar>
-            <Typography component="h1" variant="h5"
+    <Page
+      isProtected={false}
+      headContent={headContent}
+      bodyStyles={{ backgroundColor: "seagreen" }}
+    >
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            backgroundColor: "#408C93",
+            border: "2px solid rgba(255, 255, 255, .2)",
+            backdropFilter: "blur(30px)",
+            // borderWidth: '1px',
+            width: "420px",
+            borderRadius: "10px",
+            padding: "30px 40px",
+            // color: '#fff',
+            boxShadow: "0 0 10px  rgba(0, 0, 0, .2)",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "black" }}></Avatar>
+          <Typography
+            component="h1"
+            variant="h5"
             sx={{
               fontSize: "36px",
-              marginBottom: '20px',
-              color: '#fff'
-            }}>
-              Login
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleFormSubmit} sx={{ mt: 3, color: '#fff',}} > 
+              marginBottom: "20px",
+              color: "#fff",
+            }}
+          >
+            Login
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleFormSubmit}
+            sx={{ mt: 3, color: "#fff" }}
+          >
             <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    // variant="filled"
-                    id="email"
-                    label="Email"
-                    autoComplete="email"
-                    placeholder="Email"
-                    name="email"
-                    type="email"
-                    value={formState.email}
-                    onChange={handleChange}
-                    InputProps={{
-                      style: {
-                        borderRadius: "15px",
-                        color: 'white',
-                        accentColor: '#fff',
-                        backgroundColor: '#235157',
-                      }
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12} >
-                  <TextField
-                    fullWidth
-                    id="password"
-                    // variant="filled"
-                    label="Password"
-                    placeholder="Password"
-                    name="password"
-                    type="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                    autoComplete="new-password"
-                    InputProps={{
-                      style: {
-                        borderRadius: "15px",
-                        color: 'white',
-                        accentColor: '#fff',
-                        backgroundColor: '#235157',
-                      }
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                    label="Remember me" sx={{color: 'white'}}
-                  />
-                </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  // variant="filled"
+                  id="email"
+                  label="Email"
+                  autoComplete="email"
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                  InputProps={{
+                    style: {
+                      borderRadius: "15px",
+                      color: "white",
+                      accentColor: "#fff",
+                      backgroundColor: "#235157",
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="password"
+                  // variant="filled"
+                  label="Password"
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                  autoComplete="new-password"
+                  InputProps={{
+                    style: {
+                      borderRadius: "15px",
+                      color: "white",
+                      accentColor: "#fff",
+                      backgroundColor: "#235157",
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="Remember me"
+                  sx={{ color: "white" }}
+                />
+              </Grid>
             </Grid>
             {loading ? (
-              <Button  fullWidth type="submit" variant="contained" sx={{ mt: 3, mb: 2, backgroundColor: '#6DA5C0' }} disabled={true} style={styles.submitBtn}>
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: "#6DA5C0" }}
+                disabled={true}
+                style={styles.submitBtn}
+              >
                 Loading...
               </Button>
             ) : (
-              <Button fullWidth type="submit"  variant="contained" sx={{ mt: 3, mb: 2, backgroundColor: '#6DA5C0' }} style={styles.submitBtn}>
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: "#6DA5C0" }}
+                style={styles.submitBtn}
+              >
                 Login
               </Button>
             )}
-              <Grid container justifyContent="">
-                <Grid item> Don't have an account?
-                  <Link to={"/signup"} variant="body2" sx={{ color: ''}}>
+            <Grid container justifyContent="">
+              <Grid item>
+                {" "}
+                Don't have an account?
+                <Link to={"/signup"} variant="body2" sx={{ color: "" }}>
                   Register
-                  </Link>
-                </Grid>
+                </Link>
               </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Container>
-        {error && <h3>{error.message}</h3>}
-      </Page>
+        </Box>
+      </Container>
+      {error && <h3>{error.message}</h3>}
+    </Page>
   );
 }
