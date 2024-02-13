fafea5bac293fe69cbf6a0dcc0043920e5360e19
sepan mohammed <sepanmustafa@gmail.com>
2024-02-13 14:05:22 -0600

    styled the gallery page to display information that send from the dashboard page, displaying the card as closely looking as a business card

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -40,11 +40,11 @@ export default function Dashboard() {
           }}
         >
           <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <Typography variant="h4" sx={{ maxWidth: 500 }}>
+            <Typography variant="h4" sx={{ maxWidth: 500, color: '#fff', }}>
               Welcome to your job Library. Here you can store all your job
               applications in one place or add new connections.{" "}
             </Typography>
-            <Typography variant="subtitle1" sx={{ maxWidth: 400 }}>
+            <Typography variant="subtitle1" sx={{ maxWidth: 400,color: '#fff', }}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Accusantium, dolor saepe. Nulla culpa aliquid cumque impedit odio
               eaque dolore sint non quam deserunt eligendi dolor vel ea officia,
