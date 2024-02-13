8618ec2c6920165ab544cb69e573ada82eab290b
dfussell1 <daltonfussell1@gmail.com>
2024-02-12 21:52:22 -0600

    added card for jobs on gallery

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
 
 const JobCard = ({ title, company, location, salary, tags, link, applied }) => {
-  return (
-    <div className="job-card">
-      <h2>{title}</h2>
-      <p>Company: {company}</p>
-      <p>Location: {location}</p>
-      <p>Salary: {salary}</p>
-      {/* <p>Tags: {tags.join(', ')}</p> */}
-      <a href={link}>Apply</a>
-      <label>
-        <input type="checkbox" checked={applied} />
-        Applied
-      </label>
-    </div>
-  );
+    return (
+        <Card sx={{ maxWidth: 345, bgcolor: "#408C93", color: "#fff" }}>
+        <CardActionArea>
+            <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+                {title}
+            </Typography>
+            <Typography variant='body2'>
+                <p>Company: {company}</p>
+                <p>Location: {location}</p>
+                <p>Salary: {salary}</p>
+                <a href={link}>Apply</a>
+                <p>
+                <label>
+                <input type="checkbox" checked={applied} />
+                Applied
+                </label>
+                </p>
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        </Card>
+    );
 };
 
 export default JobCard;
